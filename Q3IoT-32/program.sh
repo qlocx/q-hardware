401ff1afc84bae19178f7858223bc74b06f9cb9f
mattemyoo <matias.marti@qlocx.com>
2024-05-28 12:59:55 +0200

    bump mfw

--- Q3IoT-32/program.sh
+++ Q3IoT-32/program.sh
@@ -28,7 +28,7 @@ nrfjprog --eraseall > /dev/null 2>&1
 nrfjprog --reset > /dev/null 2>&1
 nrfjprog --recover > /dev/null 2>&1
 echo "Programming mfw file..."
-program_result=$(nrfjprog --program ./mfw_nrf9160_1.3.5.zip --verify 2>&1)
+program_result=$(nrfjprog --program ./mfw_nrf9160_1.3.6.zip --verify 2>&1)
 
 if echo "$program_result" | grep "ERROR"; then
     echo -e "Error detected during flashing. Exiting script."
